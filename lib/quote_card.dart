ca4b0f7419f833c222abfb04340f55fb367f1519
Collins <shout2me@gmail.com>
2021-05-16 03:30:57 +0300

    Quote detele

--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -3,7 +3,8 @@ import 'quote.dart';
 
 class QuoteCard extends StatelessWidget {
   final Quote quote;
-  QuoteCard({this.quote});
+  final Function delete;
+  QuoteCard({this.quote, this.delete});
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +23,12 @@ class QuoteCard extends StatelessWidget {
             Text(
               quote.author,
               style: TextStyle(fontSize: 14, color: Colors.grey[800]),
+            ),
+            SizedBox(height: 8),
+            FlatButton.icon(
+              onPressed: delete,
+              label: Text('delete quote'),
+              icon: Icon(Icons.delete),
             )
           ],
         ),
